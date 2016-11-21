90b568aadb4f9254425a0a952a56fd12529a5848
Long Ang <aisk1988@gmail.com>
2016-11-21 15:29:39 +0800

    test socket.io

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var AV = require('leanengine');
 
 AV.init({
@@ -10,12 +11,24 @@ AV.init({
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
-var app = require('./app');
+var http = require('http').Server(require('./app'));
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('foo', function(msg, fn) {
+    console.log('>>>', msg);
+    fn(msg);
+  });
+});
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
-app.listen(PORT, function () {
+http.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 
   // 注册全局未捕获异常处理器
